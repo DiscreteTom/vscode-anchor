60c77ecad5550a0c378c70726093cf47383e5de9
DiscreteTom <discrete_tom@outlook.com>
2023-11-12 11:42:17 +0800

    chore: update regex

--- utils/regex-gen.ts
+++ utils/regex-gen.ts
@@ -4,9 +4,10 @@ import * as fs from "fs";
 // Usage: ts-node utils/regex-gen.ts
 
 const defaultDefinitionPattern = compose(
-  ({ concat, escape, some, capture }) =>
+  ({ concat, escape, some, capture, lookahead }) =>
     concat(
       escape("[["), // open quote
+      lookahead(escape("@"), { negative: true }), // prevent `[[@` which is a reference
       // wrap the content in the 1st capture group
       capture(
         // at least 1, non greedy to prevent matching the close quote
