8045f6360318afdc76cf451decc0b013769072fa
DiscreteTom <discrete_tom@outlook.com>
2023-11-12 14:31:59 +0800

    chore: simplify server/scan

--- src/server/scan.ts
+++ src/server/scan.ts
@@ -1,57 +1,41 @@
+import { Range } from "vscode";
 import { Position } from "vscode";
-import type { ScanPatterns } from "./regex";
-import type { NameAndPos } from "./model";
+
+export type ScanCallback = (
+  /**
+   * The name is the content of the first capture group.
+   */
+  name: string,
+  range: Range
+) => void;
 
 /**
  * Scan all definitions and references in the given content.
- * No deduplication or check is performed.
  */
-export function scanFile(
-  content: string,
-  patterns: ScanPatterns,
-  repo: {
-    defs: NameAndPos[];
-    refs: NameAndPos[];
-  }
-) {
-  const defs = [] as NameAndPos[];
-  const refs = [] as NameAndPos[];
-  content.split("\n").forEach((line, lineIndex) => {
-    scanLine(line, lineIndex, patterns, repo);
+export function scanFile(text: string, pattern: RegExp, cb: ScanCallback) {
+  text.split("\n").forEach((line, lineIndex) => {
+    scanLine(line, lineIndex, pattern, cb);
   });
-  return { defs, refs };
 }
 
 /**
  * Scan all definitions and references in the given line.
- * No deduplication or check is performed.
  */
 export function scanLine(
   line: string,
   lineIndex: number,
-  patterns: ScanPatterns,
-  repo: {
-    defs: NameAndPos[];
-    refs: NameAndPos[];
-  }
-) {
-  // find all defs
-  appendMatched(repo.defs, line, lineIndex, patterns.def);
-
-  // find all refs
-  appendMatched(repo.refs, line, lineIndex, patterns.ref);
-}
-
-export function appendMatched(
-  repo: NameAndPos[],
-  line: string,
-  lineIndex: number,
-  pattern: RegExp
+  pattern: RegExp,
+  cb: ScanCallback
 ) {
   for (const m of line.matchAll(pattern)) {
-    const name = m[1]; // the first capture group is the name
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const pos = new Position(lineIndex, m.index!);
-    repo.push({ name, pos });
+    cb(
+      m[1], // the first capture group
+      new Range(
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        new Position(lineIndex, m.index!),
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        new Position(lineIndex, m.index! + m[0].length)
+      )
+    );
   }
 }
